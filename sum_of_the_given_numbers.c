c43afdd96f9e624e5f24d9347f1bdfee83567d37
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-17 11:19:47 +0530

    #include<stdio.h> int main(){     int r,i;     scanf("%d",&r);     for(i=1;i<=r;i++){         int l,u,s,i;         scanf("%d%d",&l,&u);         s=l+u;         printf("%d ",s);
        }
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int r,i;
+    scanf("%d",&r);
+    for(i=1;i<=r;i++){
+        int l,u,s,i;
+        scanf("%d%d",&l,&u);
+        s=l+u;
+        printf("%d
+",s);
+        
+    }
+}
