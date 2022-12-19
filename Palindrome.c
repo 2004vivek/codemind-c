ec6d7c9702d202576c409582f09775f2c08e574c
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-19 15:33:42 +0530

    #include<stdio.h> int main(){     int n,q,s=0,r;     scanf("%d",&n);     q=n;     while(q!=0){         r=q%10;         s=s*10+r;         q=q/10;     }     if(s==n){         printf("True");     }     else{         printf("False");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main(){
-int n,s=0,r,q;
-        scanf("%d",&n);
-        q=n;
-        while(q!=0){
-            r=q%10;
-            s=s*10+r;
-            q=q/10;
-        }
-        if(s==n){
-            printf("Palindrome");
-        }
-        else{
-            printf("Not Palindrome");
-        }
+    int n,q,s=0,r;
+    scanf("%d",&n);
+    q=n;
+    while(q!=0){
+        r=q%10;
+        s=s*10+r;
+        q=q/10;
+    }
+    if(s==n){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }
