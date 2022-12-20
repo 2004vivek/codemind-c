dcd2f154f4bc723e8c314ebc7167eade9bc84afc
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-20 20:19:02 +0530

    #include<stdio.h> int prime(int n); int main(){     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++){         if(n%i==0){             if(!prime(i)){                 c++;             }         }     }     printf("%d",c); } int prime(int n){     int i, ct=0;     for(i=1;i<=n;i++){         if(n%i==0){             ct++;         }     }     if(ct==2){         return 1;     }     else{         return 0;     } }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int prime(int n);
+int main(){
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            if(!prime(i)){
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
+int prime(int n){
+    int i, ct=0;
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            ct++;
+        }
+    }
+    if(ct==2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
