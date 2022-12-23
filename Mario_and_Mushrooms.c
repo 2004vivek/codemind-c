eb04fedca60aba58ee36e87f52454bb269559915
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-23 18:16:47 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     if(n%3==2){         printf("SMALL");     }     else if(n%3==1){         printf("HUGE");
        }
        else{
            printf("NORMAL");
        }
    }

---
+++ Mario_and_Mushrooms.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%3==2){
+        printf("SMALL");
+    }
+    else if(n%3==1){
+        printf("HUGE");
+        
+    }
+    else{
+        printf("NORMAL");
+    }
+}
