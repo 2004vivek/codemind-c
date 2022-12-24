9304820172e32a30e26eff3318928225f9eaf53d
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-24 20:47:41 +0530

    #include<stdio.h> int main(){     int n,m,k,s;     scanf("%d%d%d",&n,&m,&k);     s=m*k;     if(n<=s){         printf("YES");     }     else{         printf("NO");     }
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,m,k,s;
+    scanf("%d%d%d",&n,&m,&k);
+    s=m*k;
+    if(n<=s){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    
+}
