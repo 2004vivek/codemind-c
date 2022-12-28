a79be6344bdaed7cd20d25091b345b5bdd04a7d3
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-28 20:50:39 +0530

    #include<stdio.h> int main(){     int unit,c;     float sc;     scanf("%d",&unit); if(unit<200){     c=unit*1.20;     sc=c+100; } else if(unit>=200 &&unit<400){     c=unit*1.50;     sc=c+100; } else if(unit>=400 && unit<600){     c=unit*1.80; } else{     c=unit*2.00; } if(unit>=400){     sc=c+0.15*c;     printf("%0.2f",sc); } else{     printf("%0.2f",sc); }
    }

--- Current_bill.c
+++ Current_bill.c
@@ -3,23 +3,26 @@ int main(){
     int unit,c;
     float sc;
     scanf("%d",&unit);
-    if(unit<200){
-        c=unit*1.20;
-    }else if(unit<400){
-        c=unit*1.50;
-    }
-    else if(unit<600){
-        c=unit*1.80;
-    }
-    else{
-        c=unit*2.00;
-    }
-    if(unit>=400){
-        sc=c+0.15*c;
-        printf("%.2f",sc);
-    }
-    else{
-        sc=c+100;
-        printf("%.2f",sc);
-    }
+if(unit<200){
+    c=unit*1.20;
+    sc=c+100;
+}
+else if(unit>=200 &&unit<400){
+    c=unit*1.50;
+    sc=c+100;
+}
+else if(unit>=400 && unit<600){
+    c=unit*1.80;
+}
+else{
+    c=unit*2.00;
+}
+if(unit>=400){
+    sc=c+0.15*c;
+    printf("%0.2f",sc);
+}
+else{
+    printf("%0.2f",sc);
+}
+    
 }
