b836661bb9185cd75b851456e0ab618b5c106a18
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-30 11:55:21 +0530

    #include<stdio.h> int main(){     int r,i;     scanf("%d",&r);     for(i=1;i<=r;i++){     int n;     scanf("%d",&n);     if(n>30){         printf("YES ");     }     else{         printf("NO ");     }     } }

---
+++ Subscription.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int r,i;
+    scanf("%d",&r);
+    for(i=1;i<=r;i++){
+    int n;
+    scanf("%d",&n);
+    if(n>30){
+        printf("YES
+");
+    }
+    else{
+        printf("NO
+");
+    }
+    }
+}
