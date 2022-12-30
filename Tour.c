d8dd47196985a4c5a56d390f5a9a1b5fa183d321
2004vivek <119566938+2004vivek@users.noreply.github.com>
2022-12-30 21:06:54 +0530

    #include<stdio.h> int main(){     int r,i;     scanf("%d",&r);     for(i=1;i<=r;i++){         int a,b,c;         scanf("%d%d",&a,&b);         c=5*a+7*b;         printf("%d ",c);
        }
    }

---
+++ Tour.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int r,i;
+    scanf("%d",&r);
+    for(i=1;i<=r;i++){
+        int a,b,c;
+        scanf("%d%d",&a,&b);
+        c=5*a+7*b;
+        printf("%d
+",c);
+        
+    }
+}
