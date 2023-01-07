3f946b1b24155bf47a55d202575aae210d1e4366
2004vivek <119566938+2004vivek@users.noreply.github.com>
2023-01-07 16:51:46 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++){     int x,y;     scanf("%d%d",&x,&y);     if(2*x>5*y){         printf("Chocolate ");     }     else if(2*x<5*y){         printf("Candy ");     }     else{         printf("Either ");     }     } }

---
+++ Prepare_for_Halloween.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(2*x>5*y){
+        printf("Chocolate
+");
+    }
+    else if(2*x<5*y){
+        printf("Candy
+");
+    }
+    else{
+        printf("Either
+");
+    }
+    }
+}
